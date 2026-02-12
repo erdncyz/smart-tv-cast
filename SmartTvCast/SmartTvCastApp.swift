e05e3758929a3c557adb61a8fa75480bf68ea752
Erdinç Yılmaz <erdinc.yilmaz@digiturk.com.tr>
2026-02-12 16:44:20 +0300

    Initial Commit

---
+++ SmartTvCast/SmartTvCastApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartTvCastApp.swift
+//  SmartTvCast
+//
+//  Created by Erdinç Yılmaz on 12.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct SmartTvCastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
